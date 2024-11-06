e6cf6f21f6ffb2f6e5588fd9eae4e7f380d894fc
firat <firat.duzgun@hotmail.com>
2024-11-06 15:24:00 +0300

    mobile first header

--- main.js
+++
@@ -1,7 +0,0 @@
-import { setupCounter } from './counter.js';
-
-const root = (document.querySelector('#app').innerHTML = `
-Hi!
-`);
-root.setupCounter();
-setupCounter(document.querySelector('#counter'));
